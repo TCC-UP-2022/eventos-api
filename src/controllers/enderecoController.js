5caac73a29718298bf4b1f0ec28aa5a1800b0479
LucasCorreiaCampos <lucascorreiacampos@gmail.com>
2022-05-02 21:01:30 -0300

    lucas-dev first version

---
+++ src/controllers/enderecoController.js
@@ -0,0 +1,24 @@
+import endereco from "../models/Endereco.js";
+
+class enderecoController {
+  static novoEndereco = (req, res) => {
+    let enderecos = new endereco(req.body);
+    enderecos.save((err, endereco) => {
+      if (err) {
+        return res.status(500).send(err);
+      } else {
+        return res.status(201).send(endereco);
+      }
+    });
+  };
+
+  static listarEndereco = (req, res) =>{
+      endereco.find((err, endereco) => {
+          err ? res.status(400).send(err) : res.status(200).json(endereco);
+      })
+  };
+
+
+};
+
+export default enderecoController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ import usuario from "../routes/usuarioRoutes.js";
 import evento from "../routes/eventoRoutes.js";
 import foto from "../routes/fotoRoutes.js";
 import checkin from "../routes/checkinRoutes.js";
+import endereco from "../routes/enderecoRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => {
     res.status(200).send("Eventos API");
   });
-  app.use(express.json(), usuario, evento, foto, checkin);
+  app.use(express.json(), usuario, evento, foto, checkin, endereco);
 };
 
 export default routes;
